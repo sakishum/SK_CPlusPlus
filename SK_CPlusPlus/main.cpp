0635697b71852e2af24b3636dc290edd946d136e
shum saki <sakishum1118@shumsMacBook-Pro.local>
2012-01-18 13:36:33 +0800

    Initial Commit

---
+++ SK_CPlusPlus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SK_CPlusPlus
+//
+//  Created by saki shum on 12-1-18.
+//  Copyright (c) 2012年 招商局物流集团. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
